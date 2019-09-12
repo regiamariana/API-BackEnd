031f49b7ccb8e197ae9734ac839f9cbd9012b7d0
regiamariana <48362221+regiamariana@users.noreply.github.com>
2019-09-12 17:32:59 -0300

    up

--- Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/LancamentosController.cs
+++ Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/LancamentosController.cs
@@ -4,12 +4,85 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Senai.OpFlix.WebApi.Domains;
+using Senai.OpFlix.WebApi.Interfaces;
+using Senai.OpFlix.WebApi.Repositories;
 
 namespace Senai.OpFlix.WebApi.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
     [ApiController]
     public class LancamentosController : ControllerBase
     {
+        private ILancamentosRepository lancamentosRepository { get; set; }
+
+        public LancamentosController()
+        {
+            lancamentosRepository = new LancamentosRepository();
+        }
+
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(lancamentosRepository.Listar());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+           
+                Lancamentos lancamentoBuscado = lancamentosRepository.BuscarPorId(id);
+                if (lancamentoBuscado == null)
+                    return NotFound();
+                return Ok();
+           
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Lancamentos lancamentos)
+        {
+            try
+            {
+                lancamentosRepository.Cadastrar(lancamentos);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new { mensagem = "Eita, erro: " + ex.Message });
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(Lancamentos lancamentos)
+        {
+            try
+            {
+                Lancamentos lancamentoBuscado = lancamentosRepository.BuscarPorId(lancamentos.Idlancamentos);
+                if (lancamentoBuscado == null)
+                    return NotFound();
+                lancamentosRepository.Atualizar(lancamentos);
+                return Ok();
+            }
+            catch (Exception ex )
+            {
+
+                return BadRequest(new { mensagem = "Ah, não. By - Pedro." });
+            }
+            
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            lancamentosRepository.Deletar(id);
+            return Ok();
+        }
     }
 }
--- Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Repositories/LancamentosRepository.cs
+++ Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Repositories/LancamentosRepository.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Senai.OpFlix.WebApi.Domains;
+using Senai.OpFlix.WebApi.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Senai.OpFlix.WebApi.Repositories
 {
-    public class LancamentosRepository
+    public class LancamentosRepository : ILancamentosRepository
     {
+        public void Atualizar(Lancamentos lancamentos)
+        {
+            using (OpFlixContext ctx = new OpFlixContext())
+            { 
+                Lancamentos lancamentoBuscado = ctx.Lancamentos.FirstOrDefault(x => x.Idlancamentos == lancamentos.Idlancamentos);
+                lancamentoBuscado.Titulo = lancamentos.Titulo;
+                ctx.Lancamentos.Update(lancamentoBuscado);
+                ctx.SaveChanges();
+
+
+            }
+
+        }
+
+        public Lancamentos BuscarPorId(int id)
+        {
+            using (OpFlixContext ctx = new OpFlixContext())
+            {
+                return ctx.Lancamentos.FirstOrDefault(x => x.Idlancamentos == id);
+            }
+        }
+
+        public void Cadastrar(Lancamentos lancamentos)
+        {
+            using (OpFlixContext ctx = new OpFlixContext())
+            {
+                ctx.Lancamentos.Add(lancamentos);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (OpFlixContext ctx = new OpFlixContext())
+            {
+                Lancamentos lancamentos = ctx.Lancamentos.Find(id);
+                ctx.Lancamentos.Remove(lancamentos);
+                ctx.SaveChanges();
+            }
+        }
+
+        public List<Lancamentos> Listar()
+        {
+            using (OpFlixContext ctx = new OpFlixContext())
+            {
+                return ctx.Lancamentos.ToList();
+            }
+        }
     }
 }
