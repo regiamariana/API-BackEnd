b3956a0da660972079e44c35cb961ffac77d37be
regiamariana <48362221+regiamariana@users.noreply.github.com>
2019-12-06 17:20:41 -0300

    up

---
+++ Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/LocalizacoesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.OpFlix.WebApi.Domains;
+using Senai.OpFlix.WebApi.Interfaces;
+using Senai.OpFlix.WebApi.Repositories;
+
+namespace Senai.OpFlix.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LocalizacoesController : ControllerBase
+    {
+        public ILocalizacoesRepository LocalizacoesRepository { get; set; }
+
+        public LocalizacoesController()
+        {
+            LocalizacoesRepository = new LocalizacoesRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Localizacoes localizacoes)
+        {
+            try
+            {
+                LocalizacoesRepository.Cadastrar(localizacoes);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(new { Mensagem = e.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(LocalizacoesRepository.Listar());
+        }
+    }
+}
---
+++ Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Interfaces/ILocalizacoesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Senai.OpFlix.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.OpFlix.WebApi.Interfaces
+{
+    public interface ILocalizacoesRepository
+    {
+        void Cadastrar(Localizacoes localizacoes);
+        List<Localizacoes> Listar();
+    }
+}
